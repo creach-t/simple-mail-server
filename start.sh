645e31d00d3766009e081330f00d5d23cd85cc30
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-03-10 17:11:10 +0100

    Mettre à jour le script de démarrage pour inclure la configuration TLS de Postfix

--- start.sh
+++ start.sh
@@ -3,6 +3,11 @@
 # Démarrer Postfix
 postfix start
 
+# Appliquer les configurations de sécurité pour Postfix
+echo "Configuration de Postfix pour le chiffrement TLS..."
+chmod +x /app/postfix-config.sh
+/app/postfix-config.sh
+
 # Attendre que Postfix soit prêt
 echo "Démarrage de Postfix..."
 sleep 2
